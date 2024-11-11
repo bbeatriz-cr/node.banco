5fed39fb928ef30319a7279df1f53770384fbb59
BEATRIZ DA SILVA CRUZ <beatriz.scruz7@senacsp.edu.br>
2024-11-11 16:42:29 -0300

    Projeto

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { Product } from '../models/Product';
-import {sequelize} from '../instances/mysql'
 import {User} from '../models/User'
 import { Op } from 'sequelize';
  
@@ -13,6 +12,7 @@ export const home = async (req: Request, res: Response)=>{
                 [Op.lt]:18
                 //last then = menor que
                 //gt = maior que 
+                //lte = menor e igual
             }
         }
     })*/
@@ -25,20 +25,12 @@ export const home = async (req: Request, res: Response)=>{
        } 
     })
 
-    //let users = await User.findAll()
-    // console.log("USUÁRIOS: ",JSON.stringify(users))
-    //criando um usuario no banco
-    //const user = User.build({
-        //name: 'Naruto',
-        //age:15
-    //})
-    //salvando usuario no banco
-
-    //const user = await User.create({
-        //name:"Smilinguido",
-        //age:15
-    //})
-    //await user.save()
+    /*salvando usuario no banco
+    const user = await User.create({
+        name:"Smilinguido",
+        age:15
+    })
+    await user.save()*/
    
    
     let users = await User.findAll()
